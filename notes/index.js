94175962d1225ac905f90520a0cb59f9744d2e1c
RS156 <rishabh.sarkar0696@gmail.com>
2023-02-25 20:09:19 +0530

    added mongo db implementation in separate file

--- notes/index.js
+++ notes/index.js
@@ -1,4 +1,4 @@
-let notes = [
+let notesx = [
     {
         id: 1,
         content: "HTML is easy",
@@ -16,9 +16,13 @@ let notes = [
     }
 ]
 
+require('dotenv').config()
 const { response, request } = require('express')
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -38,23 +42,16 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(
+        notes => {
+            response.json(notes)
+        }
+    )
+    
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find((n) => {
-        console.log(n.id, typeof n.id,id, typeof id,n.id ===id);
-        return n.id === id
-    })
-    console.log(note);
-    if(note){
-        response.json(note)
-    }
-    else{
-        response.status(404).end()
-    }
-   
+app.get('/api/notes/:id', (request, response) => { 
+      Note.findById(request.params.id).then(note =>response.json(note))
 })
 
 app.delete('/api/notes/:id',(request, response) =>{
@@ -78,13 +75,13 @@ app.post('/api/notes',(request,response) => {
     {
         return response.status(400).json({error : 'content missing'})
     }
-    const note = {
+    const note = new Note({
         content : body.content,
-        important: body.important || false,
-        id: generateId()
-    }
-    notes = notes.concat(note)
-    response.json(note)
+        important: body.important || false,      
+    })
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })    
 })
 
 const unknownEndpoint = (request, response) => {
@@ -93,7 +90,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
